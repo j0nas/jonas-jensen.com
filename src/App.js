4bf14a0ad1fe4284d0d22d66d831cd1639918676
Jonas Jensen <jonasj@mesan.no>
2016-10-10 23:53:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    Jonas Jensen
+                    <br/>
+                    <br/>
+                    <a href="mailto:jonas.jensen@msn.com">Mail</a>
+                    &nbsp;|&nbsp;
+                    <a href="https://github.com/j0nas">Github</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
