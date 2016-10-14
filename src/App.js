8af48f7b9dc355ed32eb1f68c72667bce4bfd7ee
Jonas Jensen <jonas.jensen@msn.com>
2016-10-14 23:48:23 +0200

    convert app component to functional

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, {Component} from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    Jonas Jensen
-                    <br/>
-                    <br/>
-                    <a href="mailto:jonas.jensen@msn.com">Mail</a>
-                    &nbsp;|&nbsp;
-                    <a href="https://github.com/j0nas">Github</a>
-                </div>
-            </div>
-        );
-    }
-}
+const App = () =>
+    <div className="App">
+        <div className="App-header">
+            Jonas Jensen
+            <br/>
+            <br/>
+            <a href="mailto:jonas.jensen@msn.com">Mail</a>
+            &nbsp;|&nbsp;
+            <a href="https://github.com/j0nas">Github</a>
+        </div>
+    </div>;
 
 export default App;
