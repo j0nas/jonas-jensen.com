f63657b9cc5a960a2b670994b2a3200c47e3e57b
Jonas Jensen <jonas.jensen@msn.com>
2016-10-15 23:26:51 +0200

    add Home menuitem in drawer

--- src/components/AppDrawer/index.js
+++ src/components/AppDrawer/index.js
@@ -5,6 +5,11 @@ import {Link} from "react-router";
 
 const AppDrawer = ({docked, open, setDrawerState}) =>
     <Drawer docked={docked} open={open} onRequestChange={open => setDrawerState(open)}>
+        <Link to="/" onClick={() => setDrawerState(false)}>
+            <MenuItem>
+                Home
+            </MenuItem>
+        </Link>
         <Link to="/blog" onClick={() => setDrawerState(false)}>
             <MenuItem>
                 Blog
