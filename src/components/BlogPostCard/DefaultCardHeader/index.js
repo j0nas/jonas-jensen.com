11676ebf07a7d24ca80973b385c983d1d0fcbefd
Jonas Jensen <jonas.jensen@msn.com>
2016-10-15 21:19:18 +0200

    one-line props

--- src/components/BlogPostCard/DefaultCardHeader/index.js
+++ src/components/BlogPostCard/DefaultCardHeader/index.js
@@ -3,10 +3,7 @@ import {CardHeader} from 'material-ui/Card';
 import avatar from "./images/photo.jpg";
 
 const DefaultCardHeader = ({date}) =>
-    <CardHeader
-        title={<a href="/bio">Jonas Jensen</a>}
-        subtitle={date}
-        avatar={avatar}/>;
+    <CardHeader title={<a href="/bio">Jonas Jensen</a>} subtitle={date} avatar={avatar}/>;
 
 DefaultCardHeader.propTypes = {
     date: React.PropTypes.string.isRequired,
