7c98614fb6cf62c03a10ab6ddf8b2670dd7b3f14
Jonas Jensen <jonas.jensen@msn.com>
2016-10-15 22:07:00 +0200

    [WIP] set up routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import injectTapEventPlugin from "react-tap-event-plugin";
 import {Router, Route, browserHistory} from "react-router";
 import App from "./components/App/App";
 import PostContainer from "./components/PostContainer";
+import Post from "./components/Post";
 import "./index.css";
 
 injectTapEventPlugin();
@@ -11,8 +12,9 @@ injectTapEventPlugin();
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <Route path="blog" component={PostContainer} />
-            <Route path="*" />
+            <Route path="blog" component={PostContainer}/>
+            <Route path="blog/:postId" component={Post}/>
+            <Route path="*"/>
         </Route>
     </Router>,
     document.getElementById('root')
