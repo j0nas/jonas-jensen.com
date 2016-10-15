acf7ef452bfc00643585838a5e132b37d497a3a9
Jonas Jensen <jonas.jensen@msn.com>
2016-10-15 19:50:21 +0200

    extracted post contents from BlogPostCard and generalized that component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import AppBar from 'material-ui/AppBar';
 import BlogPostCard from "./BlogPostCard";
 import "./App.css";
-
+import muiImg from "./images/mui.jpg";
 
 const App = () =>
     <div>
         <AppBar title="Home"/>
-        <BlogPostCard/>
+        <BlogPostCard
+            title={<a href='http://www.material-ui.com/'>Material UI is neat!</a>}
+            subtitle="Exploring bleeding-edge UI design"
+            img={muiImg}
+        />
     </div>;
 
 export default App;
--- src/BlogPostCard/index.js
+++ src/BlogPostCard/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import {Card, CardMedia, CardTitle} from 'material-ui/Card';
 import DefaultCardHeader from "./DefaultCardHeader";
-import muiImg from "./images/mui.jpg";
+import muiImg from "../images/mui.jpg";
 import "./style.css";
 
-const BlogPostCard = () =>
+const BlogPostCard = ({title, subtitle, img}) =>
     <div className="CardContain">
         <Card>
             <DefaultCardHeader/>
-            <CardMedia overlay={
-                <CardTitle
-                    title={<a href='http://www.material-ui.com/'>Material UI is neat!</a>}
-                    subtitle="Exploring bleeding-edge UI design"/>
-            }>
+            <CardMedia overlay={<CardTitle title={title} subtitle={subtitle}/>}>
                 <img src={muiImg} alt="React"/>
             </CardMedia>
         </Card>
     </div>;
+
+BlogPostCard.propTypes = {
+    title: React.PropTypes.node.isRequired,
+    subtitle: React.PropTypes.string.isRequired,
+    img: React.PropTypes.string.isRequired,
+};
 
 export default BlogPostCard;
