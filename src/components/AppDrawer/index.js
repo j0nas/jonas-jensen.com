4eb4cceba97ae671e1225ccd79cd1cf576500081
Jonas Jensen <jonas.jensen@msn.com>
2016-10-15 23:17:24 +0200

    made app drawer work

---
+++ src/components/AppDrawer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
+import {Link} from "react-router";
+
+const AppDrawer = ({docked, open, setDrawerState}) =>
+    <Drawer docked={docked} open={open} onRequestChange={open => setDrawerState(open)}>
+        <Link to="/blog" onClick={() => setDrawerState(false)}>
+            <MenuItem>
+                Blog
+            </MenuItem>
+        </Link>
+    </Drawer>;
+
+export default AppDrawer;
