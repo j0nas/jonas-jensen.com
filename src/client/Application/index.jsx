d366c245e9435ce2417fba1e9ac0bb9a107ef2a2
Jonas Jensen <jonasj@mesan.no>
2017-01-14 22:05:36 +0100

    deleted all unused content from boilerplate

--- src/client/Application/index.jsx
+++ src/client/Application/index.jsx
@@ -1,40 +1,24 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { middleware as reduxPackMiddleware } from 'redux-pack';
-import rootReducer from '../reducers';
 import Main from '../views/Main';
 import './style.scss';
 
-const reduxMiddleware = applyMiddleware(thunk, reduxPackMiddleware);
-
 const isDevelopmentEnvironment = process.env.NODE_ENV === 'development';
-const enhancer =
-    isDevelopmentEnvironment ?
-        compose(reduxMiddleware, ...(window.devToolsExtension ? [window.devToolsExtension()] : [])) :
-        reduxMiddleware;
-
-const store = createStore(rootReducer, enhancer);
-
 const rootElement = document.getElementById('root');
 const reactRoot = (
-  <Provider store={store}>
-    <AppContainer>
-      <Main />
-    </AppContainer>
-  </Provider>
+  <AppContainer>
+    <Main />
+  </AppContainer>
 );
 
 render(reactRoot, rootElement);
 
-if (!isDevelopmentEnvironment) {
+if (isDevelopmentEnvironment) {
+  if (module.hot) {
+    module.hot.accept('../views/Main', () =>
+      render(Object.assign({}, reactRoot), rootElement));
+  }
+} else {
   require('offline-plugin/runtime').install(); // eslint-disable-line global-require
-}
-
-if (module.hot) {
-  module.hot.accept('../views/Main', () =>
-    render(Object.assign({}, reactRoot), rootElement));
 }
