1bf57d90af60ec60e7632a221b52ec93b6279da3
Jonas Jensen <jonas.jensen@msn.com>
2017-01-21 00:33:58 +0100

    slight refactor of index.jsx

--- src/client/Application/index.jsx
+++ src/client/Application/index.jsx
@@ -4,20 +4,18 @@ import { render } from 'react-dom';
 import Main from '../views/Main';
 import './style.scss';
 
-const isDevelopmentEnvironment = process.env.NODE_ENV === 'development';
-const rootElement = document.getElementById('root');
-const reactRoot = (
-  <AppContainer>
-    <Main />
-  </AppContainer>
-);
+const renderApp = () =>
+    render(
+      <AppContainer>
+        <Main />
+      </AppContainer>,
+        document.getElementById('root'));
 
-render(reactRoot, rootElement);
+renderApp();
 
-if (isDevelopmentEnvironment) {
+if (process.env.NODE_ENV === 'development') {
   if (module.hot) {
-    module.hot.accept('../views/Main', () =>
-      render(Object.assign({}, reactRoot), rootElement));
+    module.hot.accept('../views/Main', renderApp);
   }
 } else {
   require('offline-plugin/runtime').install(); // eslint-disable-line global-require
