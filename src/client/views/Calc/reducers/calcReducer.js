6d24e7a829f39388e286a64a670505fdfd3d0592
Jonas Jensen <jonas.jensen@msn.com>
2017-05-19 17:42:46 +0200

    extract calculations to separate files, move calculation logic to reducer

--- src/client/views/Calc/reducers/calcReducer.js
+++ src/client/views/Calc/reducers/calcReducer.js
@@ -1,9 +1,42 @@
 import { UPDATE_FIELD } from '../types/calcTypes';
+import {
+  calculateExpectedReturnRate,
+  calculateTargetSum,
+  deductPercentage,
+  deductTaxPercentageFromIncome,
+  normalize,
+  savingYearsNeededForRetirement,
+} from '../util/calculations';
 
 export default (state = {}, action) => {
+  const stateCopy = { ...state, [action.name]: action.value };
+  const {
+    amountSavedSoFar,
+    income,
+    taxPercent,
+    percentOfIncomeSaved,
+    expenditures,
+    expenditureIsAnnual,
+    interestRate,
+  } = stateCopy;
+
+  const incomeAfterTax = deductTaxPercentageFromIncome(income, taxPercent);
+  const targetSum = calculateTargetSum(expenditures, expenditureIsAnnual);
+  const amountOfIncomeSaved = deductPercentage(incomeAfterTax, percentOfIncomeSaved);
+  const expectedReturnRate = calculateExpectedReturnRate(interestRate);
+  const amountOfYearsSavingNeededForRetirement =
+    savingYearsNeededForRetirement(normalize(amountSavedSoFar), targetSum, amountOfIncomeSaved, expectedReturnRate);
+
   switch (action.type) {
     case UPDATE_FIELD:
-      return { ...state, [action.name]: action.value };
+      return {
+        ...stateCopy,
+        incomeAfterTax,
+        targetSum,
+        amountOfIncomeSaved,
+        expectedReturnRate,
+        amountOfYearsSavingNeededForRetirement,
+      };
 
     default:
       return state;
