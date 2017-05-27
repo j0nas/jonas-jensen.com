2f0d81e8848b1f7d289b04df3984e78bc19a6101
Jonas Jensen <jonas.jensen@msn.com>
2017-05-28 01:27:30 +0200

    remove onDomContentLoaded listener, as it wasn't working in IE

--- js/calc.js
+++ js/calc.js
@@ -79,11 +79,7 @@ function addInputListeners(input) {
   input.addEventListener('input', inputListener);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  if (inputs) {
-    for (var inputIndex = 0; inputIndex < inputs.length - 1; inputIndex++)
-      addInputListeners(inputs[inputIndex]);
-  }
+for (var inputIndex = 0; inputIndex < inputs.length - 1; inputIndex++)
+  addInputListeners(inputs[inputIndex]);
 
-  document.getElementById('expenditureIsAnnual').addEventListener('click', inputListener);
-});
+document.getElementById('expenditureIsAnnual').addEventListener('click', inputListener);
