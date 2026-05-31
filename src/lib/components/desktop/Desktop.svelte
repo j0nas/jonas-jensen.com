0fc84e73a480c3d35b64616f699ad90cef57f6bf
Jonas Jensen <jonas.jensen@msn.com>
2026-05-31 20:44:57 +0200

    Shorten text, remove LLM slop language

--- src/lib/components/desktop/Desktop.svelte
+++ src/lib/components/desktop/Desktop.svelte
@@ -174,23 +174,9 @@
   width={apps['personal-details'].defaultSize.width}
   height={apps['personal-details'].defaultSize.height}
 >
-  <Notepad content={`========================================
-  PERSONAL DETAILS
-========================================
+  <Notepad content={`Hi, I'm Jonas. I'm a software developer based in Oslo, Noraw. I'm super passionate about computers, technology, animal rights and tinkering. Currently working on https://pep.dev.
 
-Name        : Jonas Jensen
-Location    : Oslo, Norway
-Occupation  : Passionate software developer
-
-ABOUT
------
-Hi, I'm Jonas. I'm super passionate about computers, technology, animal rights and tinkering. Currently working on https://pep.dev.
-
-I love to make people's lives better in whatever ways I can and to build things that do so. If you do too, feel free to reach out!
-
-Jonas Jensen
-jonas.jensen@msn.com
-Oslo, Norway`} />
+I love to use my skills to make people's lives better in whatever ways I can and to build things that do so. If you do too, feel free to reach out at: jonas.jensen@msn.com`} />
 </Window>
 
 <Window
